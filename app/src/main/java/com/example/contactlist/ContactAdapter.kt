05c4931c9d82f2dff6d3d99cd28c3f855885ab2f
Murtaza Pakawala <mypaka52@gmail.com>
2022-03-24 00:39:51 +1100

    made the contact list everything working fine till now front page ready

---
+++ app/src/main/java/com/example/contactlist/ContactAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.contactlist
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_view.view.*
+
+class ContactAdapter(val context: Context, val contact: List<ContactResult>): RecyclerView.Adapter<ContactAdapter.ViewHolder>(){
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_view, parent ,false);
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val contact = contact[position];
+        holder.bind(contact)
+    }
+
+    override fun getItemCount() = contact.size
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(contact: ContactResult) {
+            itemView.tvName.text = contact.name
+
+        }
+    }
+}
--- app/src/main/java/com/example/contactlist/MainActivity.kt
+++ app/src/main/java/com/example/contactlist/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.contactlist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,6 +17,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //dragged the recycler view
+        // making the layout
+        rvcontact.layoutManager = LinearLayoutManager(this);
+        //making the data
+        val data = mutableListOf<ContactResult>()
+        //making the adapter
+        val adapter = ContactAdapter(this,data);
+        rvcontact.adapter = adapter
+
 
         val retrofit =
         Retrofit.Builder()
@@ -25,6 +36,15 @@ class MainActivity : AppCompatActivity() {
         val contact = contactService.searchContact().enqueue(object : Callback<ArrayList<ContactResult>>{
             override fun onResponse(call: Call<ArrayList<ContactResult>>, response: Response<ArrayList<ContactResult>>) {
                 Log.i(TAG,"respong is $response")
+                val body = response.body()
+                if(body == null)
+                {
+                    Log.w(TAG,"Did not recieve the valid info");
+                }
+                if (body != null) {
+                    data.addAll(body)
+                }
+                adapter.notifyDataSetChanged()
             }
 
             override fun onFailure(call: Call<ArrayList<ContactResult>>, t: Throwable) {
